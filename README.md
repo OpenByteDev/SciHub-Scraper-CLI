f5acc02db88ec6e6dcbad8ac075e341729062138
OpenByte <development.openbyte@gmail.com>
2020-05-07 02:15:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SciHub Scraper CLI
+
+[![crates.io](https://img.shields.io/crates/v/scihub_scraper_cli.svg)](https://crates.io/crates/scihub_scraper_cli)
+[![Documentation](https://docs.rs/scihub_scraper_cli/badge.svg)](https://docs.rs/scihub_scraper_cli)
+[![MIT](https://img.shields.io/crates/l/scihub_scraper_cli.svg)](./LICENSE)
+
+SciHub Scraper CLI can be used to scrap paper info including its pdf url from sci-hub.
+ 
+Sci-hub domains are automatically fetched from [sci-hub.now.sh](https://sci-hub.now.sh/).
+
+## Usage
+```cmd
+$ scihub-scraper-cli --paper [doi]
+```
+
+## License
+Licensed under MIT license ([LICENSE](./LICENSE) or http://opensource.org/licenses/MIT)
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use structopt::StructOpt;
+use scihub_scraper::SciHubScraper;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    #[structopt(short, long)]
+    paper: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
+    
+    println!("Fetching paper: {}", opt.paper);
+    let mut scraper = SciHubScraper::new();
+
+    println!("Fetching base urls...");
+    match scraper.fetch_base_urls().await {
+        Ok(base_urls) => {
+            println!("Found base urls: {}", base_urls.iter().map(|e| e.url.as_str()).collect::<Vec<_>>().join(", "));
+        },
+        Err(error) => {
+            println!("Failed to fetch base urls: {}", error);
+            return;
+        }
+    }
+
+    println!("Fetching paper...");
+    match scraper.fetch_paper_by_doi(&opt.paper).await {
+        Ok(paper) => {
+            println!("Found paper:");
+            println!("{:#?}", paper);
+        },
+        Err(error) => {
+            println!("Failed to fetch paper: {}", error);
+            return;
+        }
+    }
+}
