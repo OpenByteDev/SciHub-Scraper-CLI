03daeee67c85fd391a3949067986287352d33aa6
OpenByte <development.openbyte@gmail.com>
2020-05-17 17:58:20 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use structopt::StructOpt;
 use scihub_scraper::SciHubScraper;
+use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
@@ -11,15 +11,22 @@ struct Opt {
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
-    
+
     println!("Fetching paper: {}", opt.paper);
     let mut scraper = SciHubScraper::new();
 
     println!("Fetching base urls...");
     match scraper.fetch_base_urls().await {
         Ok(base_urls) => {
-            println!("Found base urls: {}", base_urls.iter().map(|e| e.url.as_str()).collect::<Vec<_>>().join(", "));
-        },
+            println!(
+                "Found base urls: {}",
+                base_urls
+                    .iter()
+                    .map(|e| e.url.as_str())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
         Err(error) => {
             println!("Failed to fetch base urls: {}", error);
             return;
@@ -31,7 +38,7 @@ async fn main() {
         Ok(paper) => {
             println!("Found paper:");
             println!("{:#?}", paper);
-        },
+        }
         Err(error) => {
             println!("Failed to fetch paper: {}", error);
             return;
